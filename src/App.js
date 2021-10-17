c93bd54c129701fb0d59c4e47e890231836ef9ab
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-10-17 22:44:04 +0300

    fetch added

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ export default class App extends Component {
   handleFormSubmit = (keyword) => {
     console.log(keyword);
     this.setState({ keyword });
-    const APIkey = "22968833-cf9b798f42870513c2372fa03";
-    const SearchUrl = `https://pixabay.com/api/?key=${APIkey}&q=${keyword}&image_type=photo`;
-    console.log(SearchUrl);
   };
   render() {
     return (
       <div>
         <SearchBar onSubmit={this.handleFormSubmit} />
         <p>Yo're looking for: {this.state.keyword}</p>
+        <ul>
+          <ImageGalleryItem request={this.state.keyword} />
+        </ul>
         <ToastContainer autoClose={3000} />
       </div>
     );
--- src/ImageGalleryItem/ImageGalleryItem.js
+++ src/ImageGalleryItem/ImageGalleryItem.js
@@ -2,10 +2,28 @@ import { Component } from "react";
 import s from "./ImageGalleryItem.module.css";
 
 export default class ImageGalleryItem extends Component {
+  state = {
+    pictures: null,
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const APIkey = "22968833-cf9b798f42870513c2372fa03";
+    const SearchUrl = `https://pixabay.com/api/?key=${APIkey}&q=${this.props.request}&image_type=photo`;
+    if (prevProps.request !== this.props.request) {
+      fetch(SearchUrl)
+        .then((res) => res.json())
+        .then((pics) => this.setState({ pictures: pics.hits }));
+    }
+    // console.log(this.state.pictures);
+    const mapping = () => {
+      console.log(this.state.pictures);
+    };
+    mapping();
+  }
   render() {
     return (
       <li className={s.ImageGalleryItem}>
         <img src="" alt="" className={s.ImageGalleryItemPic} />
+        <p>{this.props.request}</p>
       </li>
     );
   }
