465ef5ccba2ec2dfd33431dac2a3899f0ca5e6a5
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-10-18 09:43:01 +0300

    Update ImageGalleryItem.js

--- src/ImageGalleryItem/ImageGalleryItem.js
+++ src/ImageGalleryItem/ImageGalleryItem.js
@@ -3,9 +3,9 @@ import s from "./ImageGalleryItem.module.css";
 
 export default class ImageGalleryItem extends Component {
   state = {
-    pictures: null,
+    pictures: [],
   };
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     const APIkey = "22968833-cf9b798f42870513c2372fa03";
     const SearchUrl = `https://pixabay.com/api/?key=${APIkey}&q=${this.props.request}&image_type=photo`;
     if (prevProps.request !== this.props.request) {
@@ -13,18 +13,21 @@ export default class ImageGalleryItem extends Component {
         .then((res) => res.json())
         .then((pics) => this.setState({ pictures: pics.hits }));
     }
-    // console.log(this.state.pictures);
-    const mapping = () => {
-      console.log(this.state.pictures);
-    };
-    mapping();
+
+    // const mapping = () => {
+    //   console.log(this.state.pictures);
+    // };
+    // mapping();
   }
   render() {
     return (
-      <li className={s.ImageGalleryItem}>
-        <img src="" alt="" className={s.ImageGalleryItemPic} />
-        <p>{this.props.request}</p>
-      </li>
+
+      <>
+        {this.state.pictures.map((pic) => {
+          return (<li key={pic.id} className={s.ImageGalleryItem}><img src={pic.webformatURL} alt={pic.tags} className={s.ImageGalleryItemPic} width='300' /></li>)
+        })}
+      </>
+
     );
   }
 }
