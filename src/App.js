becabf8589165a0cd739390a3c3f7e1d3e9ad478
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-10-24 19:23:51 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-
 import { Component } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import SearchBar from "./Components/SearchBar";
-import ImageGallery from "./Components/ImageGallery/ImageGallery"
+// import ImageGallery from "./Components/ImageGallery/ImageGallery"
 
 
-export default class App extends Component {
-  state = {
-    keyword: "",
-    page: null,
-    showModal: false,
-  };
-  handleFormSubmit = (keyword) => {
-    this.setState({ keyword });
-    this.setState({ page: 1 })
+function App() {
 
-  };
-  render() {
+
+    const handleFormSubmit = (keyword) => {
+        this.setState({ keyword });
+        this.setState({ page: 1 })
+
+    };
+
     return (
-      <div className='App'>
-        <SearchBar onSubmit={this.handleFormSubmit} />
-
-        <ImageGallery request={this.state.keyword} startPage={this.state.page} />
-        <ToastContainer autoClose={3000} />
-      </div>
+        <div className='App'>
+            <SearchBar onSubmit={handleFormSubmit} />
+            <ImageGallery request={this.state.keyword} startPage={this.state.page} />
+            <ToastContainer autoClose={3000} />
+        </div>
     );
-  }
 }
+
+export default App
---
+++ src/AppOld.js
@@ -0,0 +1,31 @@
+
+import { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import ImageGallery from "./Components/ImageGallery/ImageGallery"
+
+
+export default class App extends Component {
+  state = {
+    keyword: "",
+    page: null,
+    showModal: false,
+  };
+  handleFormSubmit = (keyword) => {
+    this.setState({ keyword });
+    this.setState({ page: 1 })
+
+  };
+  render() {
+    return (
+      <div className='App'>
+        <SearchBar onSubmit={this.handleFormSubmit} />
+
+        <ImageGallery request={this.state.keyword} startPage={this.state.page} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+}
